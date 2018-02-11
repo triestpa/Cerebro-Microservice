b7868a33a093e330932de36124a8f6b82c8f1245
triestpa <patrick.triest@gmail.com>
2018-02-11 13:20:04 -0500

    Add setup script to download model weights

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+mkdir models
+cd models
+# wget https://github.com/fchollet/deep-learning-models/releases/download/v0.5/inception_v3_weights_tf_dim_ordering_tf_kernels.h5
+wget https://github.com/fchollet/deep-learning-models/releases/download/v0.7/inception_resnet_v2_weights_tf_dim_ordering_tf_kernels.h5
